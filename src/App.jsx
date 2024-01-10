2696241658cec504d2c8a0c313868fdce3bf2097
Hemel17 <redewanmahmud@gmail.com>
2024-01-10 23:37:32 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+const App = () => {
+  return (
+    <div className="bg-white">
+      App
+
+
+
+
+      <div className="flex justify-center">
+        <p>
+          Challenge by
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            rel="noreferrer"
+            target="_blank"
+            className="font-bold"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by
+          <a href="https://github.com/hemel17" className="font-bold">
+            Hemel
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+export default App;
